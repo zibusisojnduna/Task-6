0fcd360fdf7df8403970d8a964425e52d9b4318d
User <User@WRSDLD309-023aa.dut.ac.za>
2024-01-26 08:34:55 +0200

    First Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+setTimeout(function(){
+    // const main1 = document.getElementById("main1")
+    // main1.style.display = "none"
+    document.getElementById("main1").style.display = "none"
+    document.getElementById("main2").style.display = "block"
+}, 3000)
