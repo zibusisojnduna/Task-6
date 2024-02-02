f9786eccceba64f8d3771483ad51b5b20e10e9ce
User <User@WRSDLD309-023aa.dut.ac.za>
2024-02-02 13:46:53 +0200

    New Changes

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ const settings = {
 	url: 'https://spotify23.p.rapidapi.com/albums/?ids=0vE6mttRTBXRe9rKghyr1l',
 	method: 'GET',
 	headers: {
-		
+		'X-RapidAPI-Key': 'a2935f3471msh03e57ecfdd61e6cp1b0c67jsn9b5aa5d546de',
+		'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
 	}
 };
 
@@ -22,12 +23,22 @@ $.ajax(settings).done(function (response) {
 	}
 	let artist = response.albums[0].artists[0].name
 	$("#artist").text(artist)
-	$("")
+
+	let album = response.albums[0].images[0].name
+	$("#album").text(album)
+	
+	let tracks = response.albums[0].tracks.items
+
+	console.log(tracks)
+
+	for(let i=0; i < tracks.length; i++){
+		
+	}
 	
 	//let album = response.albums[1].
-	console.log(response.albums)
+	//console.log(response.albums)
 	
 	
-	console.log(response);
+
 });
 
