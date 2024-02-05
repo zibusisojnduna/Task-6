3ae183b427fcc9770af7927f5fa0e28dbdf5cabb
User <User@WRSDLD309-023aa.dut.ac.za>
2024-02-05 11:56:46 +0200

    New Changes

--- script.js
+++ script.js
@@ -9,10 +9,12 @@ const settings = {
 	crossDomain: true,
 	url: 'https://spotify23.p.rapidapi.com/albums/?ids=0vE6mttRTBXRe9rKghyr1l',
 	method: 'GET',
-	headers: {
+	headers: {'X-RapidAPI-Key': 'a2935f3471msh03e57ecfdd61e6cp1b0c67jsn9b5aa5d546de',
+	'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+}
 		
 	}
-};
+
 
 $.ajax(settings).done(function (response) {
 	document.getElementById("main1").style.display = "none"
@@ -30,9 +32,37 @@ $.ajax(settings).done(function (response) {
 
 	console.log(tracks)
 
+	var text = '<tr><th><h2 style="color: white;">Track Name/<br>Length</h2></th><th></th></tr>';
+
 	for(let i=0; i < tracks.length; i++){
+
+		let time = ""
+		let minutes
+		let seconds
+		let holder = tracks[i].duration_ms
+
+		holder /= 1000
+		holder = Math.round(holder)
+
+		if( holder < 60){
+			seconds = holder
+			minutes = 0
+		}else{
+			seconds = holder % 60
+			minutes = (holder - seconds)/60
+		}
+
+		time = `${minutes}:${seconds}`
+		
+		if(i % 2 == 0){
+			text += "<tr><td><h3>" + tracks[i].name + "<br>" + time + "</h3></td>"
+		}else{
+			text += "<td><h3>" + tracks[i].name + "<br>" + time + "</h3></td></tr>"
+		}
 		
 	}
+	console.log(text)
+	$("#list").html(text)
 	
 	//let album = response.albums[1].
 	//console.log(response.albums)
